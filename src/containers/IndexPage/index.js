5d76b3ae953d05b565b6e6e8ca50bfc4efd03a19
Shawn Lin <shawn@newlife.org.tw>
2019-08-19 21:17:50 +0800

    新增圖片

--- src/containers/IndexPage/index.js
+++ src/containers/IndexPage/index.js
@@ -1,10 +1,47 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 
+
+const EkkImage = styled.img``;
+
+const NameText = styled.p`
+  margin-top: 30px;
+  font-size: 72px;
+  font-weight: 300;
+  text-align: center;
+  letter-spacing: 20px;
+  text-indent: 20px;
+  color: white;
+`;
+
+const ChurchText = styled.p`
+  font-size: 36px;
+  text-align: center;
+  letter-spacing: 1px;
+  color: white;
+`;
+
+const Logo = styled.img`
+  margin-top: 50px;
+  margin-bottom: 50px;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
 const Card = styled.div`
-  width: 480px;
-  height: 640px;
-  border: solid 1px black;
+    position: relative;
+    width: 400px;
+    height: 600px;
+    border-radius: 30px;
+    background-color: hsl(215, 62%, 21%);
+    overflow: hidden;
+
+    ${EkkImage} {
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+  }
+
 `;
 
 const A = styled.a``;
@@ -25,10 +62,12 @@ const Wrapper = styled.div`
 
 const IndexPage = () => (
   <Wrapper>
-    <Card />
-    <Link href='/about' passHref>
-      <A>About Page</A>
-    </Link>
+    <Card>
+      <Logo src='/static/logo.svg' />
+      <ChurchText>新生命小組教會</ChurchText>
+      <NameText>林盛康</NameText>
+      <EkkImage src='/static/ekk.svg' />
+  </Card>
   </Wrapper>
 );
 
